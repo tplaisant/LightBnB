1c1012c7cd0140be36055d8233374f569bc798c8
Tiago Plaisant <tiagoplaisant@hotmail.com>
2025-01-27 20:44:32 -0700

    fixing where clause when refactoring getAllProperties

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -106,37 +106,45 @@ const getAllProperties = function (options, limit = 10) {
   FROM properties
   JOIN property_reviews ON properties.id = property_id
   `;
-  queryString += `WHERE 1 = 1`;
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `WHERE owner_id = $${queryParams.length} `;
+  } else {
+    queryString += `WHERE 1 = 1`;
+  }
+  
   // 3
   if (options.city) {
     queryParams.push(`%${options.city}%`);
     queryString += `AND city LIKE $${queryParams.length} `;
   } 
-  if (options.minimum_price_per_night) {
-    queryParams.push(`${options.minimum_price_per_night}`);
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
     queryString += `AND cost_per_night >= $${queryParams.length} `;
-  }
-  if (options.maximum_price_per_night) {
-    queryParams.push(`${options.maximum_price_per_night}`);
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
     queryString += `AND cost_per_night <= $${queryParams.length} `;
   }
-  if (options.minimum_rating) {
-    queryParams.push(`${options.minimum_rating }`);
-    queryString += `AND rating >= $${queryParams.length} `;
-  }
-  if (options.owner_id) {
-    queryParams.push(`${options.owner_id}`);
-    queryString += `AND owner_id = $${queryParams.length} `;
-  }
   
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating }`);    
+    queryString += `
+    GROUP BY properties.id
+    HAVING rating >= $${queryParams.length}
+    `
+    queryParams.push(limit);
+    queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;    
+  } else {
   // 4
-  queryParams.push(limit);
-  queryString += `
-  GROUP BY properties.id
-  ORDER BY cost_per_night
-  LIMIT $${queryParams.length};
-  `;
-
+    queryParams.push(limit);
+    queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  }
   // 5
   console.log(queryString, queryParams);
 
