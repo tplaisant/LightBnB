19340a8bd062a1f786178207d4680f2a6f9f662e
Tiago Plaisant <tiagoplaisant@hotmail.com>
2025-01-27 21:46:27 -0700

    removing comments and console logs

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -22,7 +22,6 @@ const getUserWithEmail = function (email) {
   .query(
     `SELECT * FROM users WHERE email = $1`, [email])
   .then((result) => {
-    console.log(result.rows[0]);
     return result.rows[0];
   })
   .catch((err) => {
@@ -40,7 +39,6 @@ const getUserWithId = function (id) {
   .query(
     `SELECT * FROM users WHERE id = $1`, [id])
   .then((result) => {
-    console.log(result.rows[0]);
     return result.rows[0];
   })
   .catch((err) => {
@@ -59,7 +57,6 @@ const addUser = function (user) {
     `INSERT INTO users (name, email, password) 
     VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password])
   .then((result) => {
-    console.log(result.rows[0]);
     return result.rows[0];
   })
   .catch((err) => {
@@ -81,7 +78,6 @@ const getAllReservations = function (guest_id, limit = 10) {
     WHERE guest_id = $1
     LIMIT $2`, [guest_id, limit])
   .then((result) => {
-    console.log(result.rows);
     return result.rows;
   })
   .catch((err) => {
@@ -98,9 +94,9 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function (options, limit = 10) {
-  // 1
+  
   const queryParams = [];
-  // 2
+  
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
@@ -113,7 +109,6 @@ const getAllProperties = function (options, limit = 10) {
     queryString += `WHERE 1 = 1`;
   }
   
-  // 3
   if (options.city) {
     queryParams.push(`%${options.city}%`);
     queryString += `AND city LIKE $${queryParams.length} `;
@@ -137,7 +132,6 @@ const getAllProperties = function (options, limit = 10) {
     LIMIT $${queryParams.length};
     `;    
   } else {
-  // 4
     queryParams.push(limit);
     queryString += `
     GROUP BY properties.id
@@ -145,10 +139,6 @@ const getAllProperties = function (options, limit = 10) {
     LIMIT $${queryParams.length};
     `;
   }
-  // 5
-  console.log(queryString, queryParams);
-
-  // 6
   return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 
@@ -168,7 +158,6 @@ const addProperty = function (property) {
      property.cost_per_night, property.street, property.city, property.province, property.post_code, 
      property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
   .then((result) => {
-    console.log(result.rows[0]);
     return result.rows[0];
   })
   .catch((err) => {
